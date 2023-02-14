50aaf8253f9abfe82be923d5f1176a7ffe02582b
Kenta Wada <124247254+kewadafabasia@users.noreply.github.com>
2023-02-14 17:31:44 +0900

    Create exe-five-co.py

---
+++ Secret/exe-five-co.py
@@ -0,0 +1,24 @@
+{
+  "Logging": {
+    "LogLevel": {
+      "Default": "Debug",
+      "System": "Information",
+      "Microsoft": "Information"
+    }
+  },
+  "ByPassShoppingCartApi": false,
+  "AllowedHosts": "*",
+  "ApplicationInsights": {
+    "InstrumentationKey": ""
+  },
+  "B2CAuth": {
+    "ClientId": "",
+    "Authority": "",
+    "Scopes": ""
+  },
+  "ApiUrl": "http://localhost:5200/v1",
+  "ApiUrlShoppingCart": "http://localhost:3000",
+  "UseB2C": false,
+  "ImagePredictorEndpoint": "http://52.160.196.180",
+  "StorageConnectionString": "DefaultEndpointsProtocol=https;AccountName=damoaimlstorage2;AccountKey=u8VwEXsTSd+ZKBDe3BQIw1PZo+ASd/STjybXQZJhBQeCjWNhD2tI+xVjiPEH5G+Q1Xu+bHrVwkm+Mqo3nSEwOw==;EndpointSuffix=core.windows.net"
+}
